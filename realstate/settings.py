614c4016b0c037749a1c3a3a2c66d799a7f1a6a9
Guido Percú <guidopercu@gmail.com>
2018-07-22 19:36:41 -0300

    CSS layout for home page incomplete

--- realstate/settings.py
+++ realstate/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
