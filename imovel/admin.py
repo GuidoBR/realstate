15efe3e5f55a495d5aabe9ea77028b2fb0505ef6
Guido Percú <guidopercu@gmail.com>
2018-09-02 20:07:49 -0300

    Preparing admin

--- imovel/admin.py
+++ imovel/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Usuario, Endereco, Servico, Imovel, Foto, Video, Locacao, Propriedade
+
+admin.site.register(Usuario)
+admin.site.register(Endereco)
+admin.site.register(Servico)
+admin.site.register(Imovel)
+admin.site.register(Foto)
+admin.site.register(Video)
+admin.site.register(Locacao)
+admin.site.register(Propriedade)
