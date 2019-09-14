f2f0318c8e01463ff8896e210bc37f814723fa8e
Guido Percú <guidopercu@gmail.com>
2019-09-14 12:37:53 -0300

    Using pipenv instead of old requirements

--- realstate/settings/settings.py
+++ realstate/settings/settings.py
@@ -77,11 +77,13 @@ WSGI_APPLICATION = 'realstate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': '127.0.0.1',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': os.environ['DB_NAME'],
+        # 'USER': os.environ['DB_USER'],
+        # 'PASSWORD': os.environ['DB_PASSWORD'],
+        # 'HOST': '127.0.0.1',
     }
 }
 
